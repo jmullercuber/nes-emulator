8d23118f23028035d05a541b60ed5c84fef4a37b
Joey Muller <jmullercuber@gmail.com>
2019-12-15 19:56:14 -0700

    determine file type from header

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 )
+
+type ROMHeader [16]byte
+
+var knownFileTypes = [2]string{"iNES", "NES 2.0"}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -15,16 +20,25 @@ func main() {
 	var input_rom_path = os.Args[1]
 	fmt.Println("Reading ROM:", input_rom_path)
 
-	var rom_bytes, err = LoadBinaryFile(input_rom_path)
+	var rom_bytes, err = loadBinaryFile(input_rom_path)
 	if err != nil {
 		panic("Unable to read ROM file")
 	}
 
 	fmt.Printf("% x\n", rom_bytes[:16])
 	fmt.Printf("% x\n", rom_bytes[16:32])
+
+	var header = getHeader(rom_bytes)
+	var file_type = determineFileType(header)
+	fmt.Println("File type:", file_type)
+	if file_type == "iNES" {
+		// processINES(header, rom_bytes)
+	} else {
+		panic("Cannot process file type: " + file_type)
+	}
 }
 
-func LoadBinaryFile(file_path string) ([]byte, error) {
+func loadBinaryFile(file_path string) ([]byte, error) {
 	file, err := os.Open(file_path)
 	if err != nil {
 		return nil, err
@@ -46,4 +60,22 @@ func LoadBinaryFile(file_path string) ([]byte, error) {
 	}
 
 	return file_bytes, nil
+}
+
+func getHeader(file_bytes []byte) ROMHeader {
+	var header ROMHeader
+	copy(header[:], file_bytes[:16])
+	return header
+}
+
+func determineFileType(header ROMHeader) string {
+	var iNESFormatStart = []byte{0x4e, 0x45, 0x53, 0x1a}
+	if bytes.Equal(header[0:4], iNESFormatStart) {
+		if (header[7] & 0x0C) == 0x08 {
+			return "NES 2.0"
+		} else {
+			return "iNES"
+		}
+	}
+	return "Unknown"
 }
